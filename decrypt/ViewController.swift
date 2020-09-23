a22ede9c5617eb44f05bc3cb3a5151fb7f2bc188
Guilherme Rangel <gui.vg16@hotmail.com>
2020-09-23 02:54:32 -0300

    add encryptedText

---
+++ decrypt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  decrypt
+//
+//  Created by Guilherme Rangel on 23/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(EncryptedText.textEncrypted.count)
+    }
+
+
+}
+
