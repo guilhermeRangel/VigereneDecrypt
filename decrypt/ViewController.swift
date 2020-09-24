367c6d33cfb31ad8acf14ab0edc11421b1575bd4
Guilherme Rangel <gui.vg16@hotmail.com>
2020-09-24 06:24:01 -0300

    kasinsk force brut

--- decrypt/ViewController.swift
+++ decrypt/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     var cipherText = ""
     let alphabet = Alphabet.alphabet
     var text:[String] = []
-    var displacement:[Int] = []
+    var displacementTrigram:[String:[Int]] = [:]
     override func viewDidLoad() {
         super.viewDidLoad()
         //le o arquivo com o texto cifrado
@@ -21,40 +21,76 @@ class ViewController: UIViewController {
         for t in cipherText{
             text.append(String(t))
         }
-        displacement = kasinsk(text: text, tamKey: 2)
-        print(displacement)
+       let last = kasinsk(text: text, tamKey: 2)
+        
+        displacementTrigram = last.last!
+        
     }
     
     
-    func kasinsk(text: [String], tamKey: Int) -> [Int]{
-        var strKey = ""
+    func kasinsk(text: [String], tamKey: Int) -> [[String:[Int]]]{
+        var trigram = ""
         var strAux = ""
-        
+        var displacementTrigram:[String:[Int]] = [:]
+        var list:[[String:[Int]]] = [[:]]
         var displacement:[Int] = []
         
-        
-        //cria uma string com tamanho fixo
-        for i in 0...tamKey{
-            strKey.append(text[i])
-        }
-        
-        //percorre todo o arquivo procurando ocorrencias iguais de strKey
-        for i in tamKey...text.count{
-            //sequencialmente pega os proximos n caracteres
-            for j in i+1...i+tamKey+1{
+        for k in 0...text.count
+    {
+            //cria o trigram
+            for i in k...k+tamKey{
+                //evita que saia do array
+                if i < text.count{
+                    trigram.append(text[i])
+                }else{
+                    return list
+                }
+            }
+            //evita que trigrams se repita a frente
+            if list.last!.keys.contains(trigram){
+                trigram.removeAll()
+            }
+            var flag = false
+            if !trigram.isEmpty {
+            //varre a estrutura logo após trigram anterior
+        for i in k+tamKey...text.count{
+            //monta o proximo trigram
+            for j in i+1...i+1+tamKey{
                 if j < text.count{
                     strAux.append(text[j])
                 }
-
             }
-            //verifica se a strKey é igual a uma string de mesmo tamanho
-            if strKey.contains(strAux){
-                //guarda a posicao que foi encontrado para calcular o deslocamento
-                displacement.append(i+1-tamKey) //passa a posicao inicial da string igual
+           
+            //compara se sao iguais
+            if trigram.contains(strAux){
+                flag = true
+                //guarda as posicoes que houve repeticao
+                displacement.append(i+1)
+                displacementTrigram.updateValue(displacement, forKey: trigram)
             }
             strAux.removeAll()
     }
-        return displacement
+            //evita que seja adicionado um trigram sem repeticao
+            if flag {
+                list.append(displacementTrigram)
+                flag = false
+            }
+            
+            trigram.removeAll()
+            displacement.removeAll()
+    }
+    }
+        return list
 }
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
